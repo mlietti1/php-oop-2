9722ff30abce3caf36c3799ead353bac59be2a41
mlietti1 <mcf.lietti@outlook.com>
2022-12-12 19:33:00 +0100

    created toy and accessory arrays

--- Database/db.php
+++ Database/db.php
@@ -18,4 +18,24 @@ $foods = [
 
 ];
 
-$toys = [];
+$toys = [
+  new Toy(5, 'KONG Dotz Circle', 'KONG', new Category('dog', 'fa-dog'), '7,56', 'https://arcaplanet.vtexassets.com/arquivos/ids/207398/Kong-Dotz-Circle-Small.jpg?v=1757249547', 'chewing', 'S'),
+
+  new Toy(6, 'KONG Scrunch Knots Raccoon', 'KONG', new Category('dog', 'fa-dog'), '12,90', 'https://arcaplanet.vtexassets.com/arquivos/ids/258241/scrunch-knots-racoon1.jpg?v=1757034748', 'soft', 'M'),
+
+  new Toy(7, 'Flopping Fish Cat Toy', 'Trixie', new Category('cat', 'fa-cat'), '8,20', 'https://arcaplanet.vtexassets.com/arquivos/ids/264784/trixie-pesce-guizzante-gioco-gatto.jpg?v=1757207679', 'moving', 'any'),
+
+  new Toy(8, 'Laser Pointer Cat Toy', 'Ferplast', new Category('cat', 'fa-cat'), '3,85', 'https://arcaplanet.vtexassets.com/arquivos/ids/206799/GIOCO-GATTO-CAT-POINTER-A-LED.jpg?v=1757265453', 'chasing', 'any')
+
+];
+
+$accessories = [
+  new Accessory(9, 'Comfort Bed Tartan', 'Lovedi', new Category('dog', 'fa-dog'), '47,90', 'https://arcaplanet.vtexassets.com/arquivos/ids/225157/merci-cane-cuccia-comfort-tartan.jpg?v=1757207376', 'fabric', 'L'),
+
+  new Accessory(10, 'Nylon Collar Red', 'Croci', new Category('dog', 'fa-dog'), '5,80', 'https://arcaplanet.vtexassets.com/arquivos/ids/220938/croci-collare-cane-nylon-rosso.jpg?v=1757121274', 'nylon', 'M'),
+
+  new Accessory(11, 'Cat Tree Baladona Beige', 'Trixie', new Category('cat', 'fa-cat'), '50,00', 'https://arcaplanet.vtexassets.com/arquivos/ids/223801/trixie-gatto-tiragraffi-badalona-beige.jpg?v=1757121092', 'synthetic', 'any'),
+
+  new Accessory(12, 'Gipsy Cat Carrier', 'Croci', new Category('cat', 'fa-cat'), '8,67', 'https://arcaplanet.vtexassets.com/arquivos/ids/227428/croci-gipsy-cane-trasportino-small.jpg?v=1757207738', 'plastic', 'any')
+
+];
--- Views/layout/head.php
+++ Views/layout/head.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.css' integrity='sha512-FA9cIbtlP61W0PRtX36P6CGRy0vZs0C2Uw26Q1cMmj3xwhftftymr0sj8/YeezDnRwL9wtWw8ZwtCiTDXlXGjQ==' crossorigin='anonymous' />
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
   <link rel="stylesheet" href="/style/style.css">
   <title>Pet Store Boolean</title>
